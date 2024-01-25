b5a5a6af3f4cdb2cac092efa47c838527a34acfc
hemankumar-ibz <hemant.sahu@interbizconsulting.com>
2024-01-26 01:08:09 +0530

    added route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 // app-routing.module.ts
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthGuard } from '../service/auth.guard';
-import { LoginComponent } from '../Auth-process/login/login.component';
-import { ListComponent } from '../list/list.component';
+import { AuthGuard } from './service/auth.guard';
+import { LoginComponent } from './Auth-process/login/login.component';
+import { ListComponent } from './list/list.component';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent },
+  { path: '', component: LoginComponent },
   { path: 'dashboard', component: ListComponent, canActivate: [AuthGuard] },
   // ... other routes
 ];
